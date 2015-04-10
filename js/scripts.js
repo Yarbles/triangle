e998df5cf52355195b5a1ba842a4a0372c9f1cc6
Reid Baldwin <student@epicodus.com>
2015-04-10 11:25:16 -0700

    write scripts code for webpage

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,16 @@ var triangulator = function(side1, side2, side3) {
         return "Scalene triangle";
     }
 };
+
+$(document).ready(function() {
+    $("form#triangulator").submit(function(event) {
+        var side_a = parseInt($("input#side_a").val());
+        var side_b = parseInt($("input#side_b").val());
+        var side_c = parseInt($("input#side_c").val());
+
+        var result = triangulator(side_a, side_b, side_c);
+
+        $("#result").show();
+        event.preventDefault();
+    });
+});
