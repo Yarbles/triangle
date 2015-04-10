392357c23085dc5d1aa9faaef1d79a3ebbafb6f7
Reid Baldwin <student@epicodus.com>
2015-04-10 12:28:35 -0700

    finish webpage functionality, change tests and scripts to conform to hilarious terminator theme

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,30 @@
 var triangulator = function(side1, side2, side3) {
-    var sum = side1 + side2 + side3;
 
     if(side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2) {
-        return "That isn't a triangle, my friend";
+        return "DAS IST KEIN TRIANGLE!!!";
     }
     else if(side1 === side2 && side2 === side3) {
-        return "Equilateral triangle";
+        return "DAS IST EQUILATERAL TRIANGLE!!!";
     }
     else if((side1 === side2 && side2 !== side3) || (side1 === side3 && side2 !== side3) || (side2 === side3 && side1 !== side3)) {
-        return "Isoceles triangle";
+        return "DAS IST ISOCELES TRIANGLE!!!";
     }
     else if(side1 !== side2 && side2 !== side3) {
-        return "Scalene triangle";
+        return "DAS IST SCALENE TRIANGLE!!!";
     }
 };
 
 $(document).ready(function() {
     $("form#triangulator").submit(function(event) {
-        var side_a = parseInt($("input#side_a").val());
-        var side_b = parseInt($("input#side_b").val());
-        var side_c = parseInt($("input#side_c").val());
-
+        var side_a = parseInt($("input#sidea").val());
+        var side_b = parseInt($("input#sideb").val());
+        var side_c = parseInt($("input#sidec").val());
         var result = triangulator(side_a, side_b, side_c);
 
+        $(".result").text(result);
+
         $("#result").show();
+
         event.preventDefault();
     });
 });
