5873c34a0d8b79ddf535db463c32e191d38126fc
Reid Baldwin <student@epicodus.com>
2015-04-10 10:45:56 -0700

    pass second test

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 var triangulator = function(side1, side2, side3) {
     var sum = side1 + side2 + side3;
-    if(side1 === side2 && side2 === side3) {
+
+    if(side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2) {
+        return "That isn't a triangle, my friend";
+    }
+    else if(side1 === side2 && side2 === side3) {
         return "Equilateral triangle";
     }
 };
--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,9 @@ describe('triangulator', function() {
     it("returns an equilaterial triangle if all the 3 sides are equal", function() {
         expect(triangulator(3, 3, 3)).to.equal("Equilateral triangle");
     });
+
+    it("returns a negative result for input that fails to meet the definition that 1 side cannot be greater than or equal to the other sides combined", function() {
+        expect(triangulator(3, 3, 20)).to.equal("That isn't a triangle, my friend");
+    })
+
 });
