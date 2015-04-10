66185b721b3f4c0a385b6d66ed46d6025193cfd9
Reid Baldwin <student@epicodus.com>
2015-04-10 11:23:07 -0700

    pass third test, isoceles

--- specs/specs.js
+++ specs/specs.js
@@ -3,8 +3,12 @@ describe('triangulator', function() {
         expect(triangulator(3, 3, 3)).to.equal("Equilateral triangle");
     });
 
-    it("returns a negative result for input that fails to meet the definition that 1 side cannot be greater than or equal to the other sides combined", function() {
+    it("returns a negative result for input that fails to meet the definition of a triangle: that 1 side cannot be greater than or equal to the other sides combined", function() {
         expect(triangulator(3, 3, 20)).to.equal("That isn't a triangle, my friend");
-    })
+    });
+
+    it("returns an isoceles triangle if exactly 2 sides are equal", function() {
+        expect(triangulator(2, 2, 3)).to.equal("Isoceles triangle");
+    });
 
 });
